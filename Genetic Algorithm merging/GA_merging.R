4fa70107eaa163c96ce1418d2b3ebf299408aa97
NickMeim <48244638+NickMeim@users.noreply.github.com>
2019-03-06 11:24:34 +0200

    Update GA_merging.R

--- Genetic Algorithm merging/GA_merging.R
+++ Genetic Algorithm merging/GA_merging.R
@@ -18,41 +18,41 @@ weights_actives_up <- readRDS("weights_active_up.rds")
 ordered_actives_down <- readRDS("ordered_active_down.rds")
 weights_actives_down <- readRDS("weights_active_down.rds")
 
-ordered_iactives <- readRDS("ordered_inactive_downanalysis.rds")
+ordered_inactives <- readRDS("ordered_inactive_downanalysis.rds")
 weights_inactives <- readRDS("weights_inactive_downanalysis.rds")
 
 
 #Rank Aggregation with GA
 
-GAW_act <- RankAggreg(ordered_actives_up, 319 ,weights_actives_up, method= "GA",
+GAW_act <- RankAggreg(ordered_actives_up, NCOL(ordered_actives_up) ,weights_actives_up, method= "GA",
                       distance="Spearman", seed=123, maxIter = 10000,
                       CP=.4, MP=.02, verbose=TRUE)
 
 GAW_act_up <- GAW_act[["top.list"]]
 final_act_up <- as.data.frame(GAW_act_up)
-final_act_up <- final_act_up %>% mutate(rank=c(1:319))
+final_act_up <- final_act_up %>% mutate(rank=c(1:NCOL(ordered_actives_up)))
 colnames(final_act_up) <- c("drugs","rank.act")
 saveRDS(final_act_up,"final_act_up.rds")
 
-GAW_act <- RankAggreg(ordered_actives_down, 448 ,weights_actives_down, method= "GA",
+GAW_act <- RankAggreg(ordered_actives_down, NCOL(ordered_actives_down) ,weights_actives_down, method= "GA",
                       distance="Spearman", seed=123, maxIter = 10000,
                       CP=.4, MP=.02, verbose=TRUE)
 
 GAW_act_down <- GAW_act[["top.list"]]
 final_act_down <- as.data.frame(GAW_act_down)
-final_act_down <- final_act_down %>% mutate(rank=c(1:448))
+final_act_down <- final_act_down %>% mutate(rank=c(1:NCOL(ordered_actives_down)))
 colnames(final_act_down) <- c("drugs","rank.act")
 saveRDS(final_act_down,"final_act_down.rds")
 
 #In inactives we reverse the ranks in the end
 
-GAW_inact <- RankAggreg(ordered_inactives, 435, weights_inactives, method= "GA",
+GAW_inact <- RankAggreg(ordered_inactives, NCOL(ordered_inactives), weights_inactives, method= "GA",
                         distance="Spearman", seed=123, maxIter = 10000,
                         CP=.4, MP=.02, verbose=TRUE)
 
 GAW_inact <- GAW_inact[["top.list"]]
 final_inact <- as.data.frame(GAW_inact)
-final_inact <- final_inact %>% mutate(rank=c(1:435))
+final_inact <- final_inact %>% mutate(rank=c(1:NCOL(ordered_inactives)))
 final_inact <- final_inact %>% mutate(rank=max(rank)-rank+1)
 final_inact <- final_inact[order(final_inact$rank),]
 colnames(final_inact) <- c("drugs","rank.inact")

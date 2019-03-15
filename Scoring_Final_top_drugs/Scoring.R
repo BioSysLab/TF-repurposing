1ca7d28bce75c2760b5a3d4c53cda34e69b18ff7
NickMeim <48244638+NickMeim@users.noreply.github.com>
2019-03-15 17:47:59 +0200

    Update Scoring.R

--- Scoring_Final_top_drugs/Scoring.R
+++ Scoring_Final_top_drugs/Scoring.R
@@ -14,29 +14,19 @@ library(RankAggreg)
 
 #WARNING: For borda the final merged lists need reversing the ranks in inactives (in GA it is already done)
 
-final_act_up<- readRDS("final_act_up.rds")
-final_act_down<- readRDS("final_act_down.rds")
-final_inact<- readRDS("final_inact.rds")
+final_up<- readRDS("final_up.rds")
+final_down<- readRDS("final_down.rds")
 
-int_down <-  Reduce(intersect,list(final_inact$drugs,
-                                   final_act_down$drugs))
-DrugScores_int_down <-  left_join(final_inact,final_act_down, by="drugs")
-DrugScores_int_down <-DrugScores_int_down %>% filter(!is.na(DrugScores_int_down$rank.inact))
-DrugScores_int_down <-DrugScores_int_down %>% filter(!is.na(DrugScores_int_down$rank.act))
-DrugScores_int_down <- DrugScores_int_down %>% mutate(score=rank.act+rank.inact)
-DrugScores_int_down$score <- DrugScores_int_down$score/max(DrugScores_int_down$score)*100
-DrugScores_int_down <- DrugScores_int_down[order(DrugScores_int_down$score),]
-saveRDS(DrugScores_int_down,"drugs_down_gaw.rds")
-write.csv(DrugScores_int_down,"drugs_down_gaw.csv")
 
-int_up <-  Reduce(intersect,list(final_inact$drugs,
-                                 final_act_down$drugs))
-DrugScores_int_up <-  left_join(final_inact,final_act_up, by="drugs")
-DrugScores_int_up <-DrugScores_int_up %>% filter(!is.na(DrugScores_int_up$rank.inact))
-DrugScores_int_up <-DrugScores_int_up %>% filter(!is.na(DrugScores_int_up$rank.act))
-DrugScores_int_up <- DrugScores_int_up %>% mutate(score=rank.act+rank.inact)
-DrugScores_int_up$score <- DrugScores_int_up$score/max(DrugScores_int_up$score)*100
-DrugScores_int_up <- DrugScores_int_up[order(DrugScores_int_up$score),]
-saveRDS(DrugScores_int_up,"drugs_up_gaw.rds")
-write.csv(DrugScores_int_up,"drugs_up_gaw.csv")
+int <-  Reduce(intersect,list(final_up$drugs,
+                                   final_down$drugs))
+DrugScores_int <-  left_join(final_down,final_up, by="drugs")
+DrugScores_int <-DrugScores_int %>% filter(!is.na(DrugScores_int$rank.down))
+DrugScores_int <-DrugScores_int %>% filter(!is.na(DrugScores_int$rank.up))
+DrugScores_int <- DrugScores_int %>% mutate(score=rank.up+rank.down)
+DrugScores_int$score <- DrugScores_int$score/max(DrugScores_int$score)*100
+DrugScores_int <- DrugScores_int[order(DrugScores_int$score),]
+saveRDS(DrugScores_int,"drugs_score_borda.rds")
+write.csv(DrugScores_int,"drugs_score_borda.csv")
+
 

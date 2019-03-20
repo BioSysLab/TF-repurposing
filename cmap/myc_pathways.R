f7414197e637585363f62b4d0e6506da70f1c805
NickMeim <48244638+NickMeim@users.noreply.github.com>
2019-03-20 19:16:48 +0200

    Create myc_pathways.R

---
+++ cmap/myc_pathways.R
@@ -0,0 +1,58 @@
+library(fgsea)
+library(gage)
+library(EGSEAdata)
+#library(KEGGREST)
+library(AnnotationDbi)
+library(org.Hs.eg.db)
+library(tidyverse)
+#library(cmapR)
+#library(rhdf5)
+#library(CARNIVAL)
+#library(viper)
+library(dplyr)
+#data(kegg.gs)
+
+#Load inputs----------------------------------------------------------------------------------
+cmap_PA<- readRDS("cmap_PA.rds")
+landmark <- read_tsv(file = "cmap_landmark_genes.txt")
+egsea.data(species = "human",returnInfo = TRUE)
+rownames(cmap_PA) <- landmark$`Entrez ID`
+rownames(cmap_PA) <- as.character(rownames(cmap_PA))
+cmap_myc_ranks_landmark<- readRDS("cmap_myc_ranks_landmark.rds")
+cmap_myc_ranks_landmark <- cmap_myc_ranks_landmark %>% filter(myc_ranks<=18)
+
+##VECTORIZED APPROACH (AGNOISE TO APLA YPARXEI GIA KANA TEST) -------------------------------------------------------------------------
+
+#Gene set enrichment analysis
+paths <- apply(cmap_PA[,1:2], MARGIN=2, fgsea, pathways= kegg.pathways$human$kg.sets, nperm=1000, minSize = 10, maxSize = 500)
+fgseaRes <- fgsea(pathways = kegg.pathways$human$kg.sets, 
+                  stats = cmap_PA[,1],
+                  minSize=10,
+                  maxSize=500,
+                  nperm=1000)
+
+
+
+##LOOP APPROACH-------------------------------------------------------------------------------
+cmap_myc_ranks_landmark$pvalue.cellcycle <- 666
+cmap_myc_ranks_landmark$pvalue.apopt <- 666
+cmap_myc_ranks_landmark$padj.cellcycle <- 666
+cmap_myc_ranks_landmark$padj.apopt <- 666
+cmap_myc_ranks_landmark$rankES.cellcycle <- 666
+cmap_myc_ranks_landmark$rankES.apopt <- 666
+for (i in 1:NCOL(cmap_PA)) {
+  paths <- fgsea(pathways = kegg.pathways$human$kg.sets, 
+                    stats = cmap_PA[,i],
+                    minSize=10,
+                    maxSize=500,
+                    nperm=10000)
+  paths <- paths %>% mutate(ES.rank=rank(ES)) %>% select(pathway,pval,padj,ES.rank)
+  cmap_myc_ranks_landmark$pvalue.cellcycle[which(cmap_myc_ranks_landmark$sig_id==colnames(cmap_PA)[i])]<-  paths %>% filter(pathway=="hsa04110 Cell cycle") %>% select(pval)
+  cmap_myc_ranks_landmark$pvalue.apopt[which(cmap_myc_ranks_landmark$sig_id==colnames(cmap_PA)[i])]<-  paths %>% filter(pathway=="hsa04210 Apoptosis") %>% select(pval)
+  cmap_myc_ranks_landmark$padj.apopt[which(cmap_myc_ranks_landmark$sig_id==colnames(cmap_PA)[i])]<-  paths %>% filter(pathway=="hsa04210 Apoptosis") %>% select(padj)
+  cmap_myc_ranks_landmark$padj.cellcycle[which(cmap_myc_ranks_landmark$sig_id==colnames(cmap_PA)[i])]<-  paths %>% filter(pathway=="hsa04110 Cell cycle") %>% select(padj)
+  cmap_myc_ranks_landmark$rankES.apopt[which(cmap_myc_ranks_landmark$sig_id==colnames(cmap_PA)[i])]<-  paths %>% filter(pathway=="hsa04210 Apoptosis") %>% select(ES.rank)
+  cmap_myc_ranks_landmark$rankES.cellcycle[which(cmap_myc_ranks_landmark$sig_id==colnames(cmap_PA)[i])]<-  paths %>% filter(pathway=="hsa04110 Cell cycle") %>% select(ES.rank)
+  paths <- NULL
+  }
+
